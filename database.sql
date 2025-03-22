6874a2c47a2665d764b27025be2df253b868bdd5
Benquerer <andrebenquerer@gmail.com>
2025-03-22 00:26:46 +0000

    Update database.sql
    Added temporary data to sql script

--- database.sql
+++ database.sql
@@ -34,4 +34,63 @@ ALTER TABLE UC_Turma ADD CONSTRAINT FKUC_Turma452449 FOREIGN KEY (Cod_Turma) REF
 ALTER TABLE UC ADD CONSTRAINT FKUC184900 FOREIGN KEY (Cod_Curso) REFERENCES Curso (Cod_Curso);
 ALTER TABLE Docente_UC ADD CONSTRAINT FKDocente_UC600484 FOREIGN KEY (ID_Docente) REFERENCES Docente (ID);
 ALTER TABLE Docente_UC ADD CONSTRAINT FKDocente_UC929250 FOREIGN KEY (Cod_UC) REFERENCES UC (Cod_UC);
+
+
+
+#======= Temporary data =======#
+
+#=== Docentes
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (1, 'João', 'docente-1@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (2, 'Maria', 'docente-2@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (3, 'Carlos', 'docente-3@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (4, 'Ana', 'docente-4@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (5, 'Pedro', 'docente-5@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (6, 'Sofia', 'docente-6@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (7, 'Rui', 'docente-7@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (8, 'Inês', 'docente-8@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (9, 'Miguel', 'docente-9@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (10, 'Beatriz', 'docente-10@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (11, 'Tiago', 'docente-11@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (12, 'Rita', 'docente-12@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (13, 'André', 'docente-13@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (14, 'Patrícia', 'docente-14@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (15, 'Gonçalo', 'docente-15@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (16, 'Vanessa', 'docente-16@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (17, 'Bruno', 'docente-17@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (18, 'Catarina', 'docente-18@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (19, 'Fábio', 'docente-19@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (20, 'Marta', 'docente-20@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (21, 'Luís', 'docente-21@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (22, 'Helena', 'docente-22@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (23, 'Ricardo', 'docente-23@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (24, 'Cláudia', 'docente-24@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (25, 'Hugo', 'docente-25@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (26, 'Joana', 'docente-26@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (27, 'Daniel', 'docente-27@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (28, 'Teresa', 'docente-28@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (29, 'Eduardo', 'docente-29@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (30, 'Lara', 'docente-30@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (31, 'Vítor', 'docente-31@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (32, 'Carla', 'docente-32@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (33, 'Samuel', 'docente-33@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (34, 'Isabel', 'docente-34@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (35, 'Nuno', 'docente-35@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (36, 'Raquel', 'docente-36@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (37, 'Paulo', 'docente-37@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (38, 'Tânia', 'docente-38@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (39, 'Fernando', 'docente-39@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (40, 'Susana', 'docente-40@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (41, 'Diogo', 'docente-41@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (42, 'Alexandra', 'docente-42@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (43, 'Marco', 'docente-43@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (44, 'Daniela', 'docente-44@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (45, 'Sérgio', 'docente-45@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (46, 'Mariana', 'docente-46@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (47, 'Jorge', 'docente-47@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (48, 'Liliana', 'docente-48@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (49, 'Artur', 'docente-49@ipt.pt', '1234');
+INSERT INTO Docente (ID, Nome, Email, Password) VALUES (50, 'Sara', 'docente-50@ipt.pt', '1234');
+
+
+
 #========================================================#
