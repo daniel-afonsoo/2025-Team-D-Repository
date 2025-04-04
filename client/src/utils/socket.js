a39a36d5db364bd090b0b2ea1a1a60311f462b8f
Benquerer <andrebenquerer@gmail.com>
2025-04-04 16:12:37 +0100

    Socket Fix
    Added handling of errors in clientside

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -8,4 +8,12 @@ socket.on("connection-ack-alert", (data) => {
     alert(data)
 })
 
+socket.on("add-aula-error", (data) => {
+    alert(data)
+})
+
+socket.on("remove-aula-error", (data) => {
+    alert(data)
+})
+
 export default socket;
