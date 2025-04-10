f2de213e385026122a00426e8d546ba4b7418c0d
Guilherme Simões <137619185+GuilhermeSimoes1@users.noreply.github.com>
2025-04-10 18:36:39 +0100

    Importação dos componentes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,18 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import CursoEdit from './components/edit_remove/Curso_edit_remove.jsx'
 import  DocenteEdit from './components/edit_remove/Docente_edit_remove.jsx'
+import CursoCreate from './components/create/CursoCreate.jsx'
+import DocenteCreate from './components/create/DocenteCreate.jsx'  
+import EscolaCreate from './components/create/EscolaCreate.jsx'  
+import UCCreate from './components/create/UCCreate.jsx'
 import  EscolaEdit from './components/edit_remove/Escola_edit_remove.jsx'
 import  UcEdit from './components/edit_remove/UC_edit_remove.jsx'
 import  SalaEdit from './components/edit_remove/Sala_edit_remove.jsx'
 import  Horarios from './components/horarios.jsx'
+import Login from './pages/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SalaEdit/>
+    {/* <Login />   */}
+    {/* <SalaEdit/>
     <UcEdit />
     <EscolaEdit />
     <DocenteEdit />
     <CursoEdit />
-    <Horarios />
+    <DocenteCreate/>  
+    <EscolaCreate/>
+    <CursoCreate/>
+    <UCCreate/>  */}
+    <Horarios /> 
   </StrictMode>,
 )
