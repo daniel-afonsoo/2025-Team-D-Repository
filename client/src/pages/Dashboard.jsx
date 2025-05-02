7ec7b6a2176e2daa6916fba0d08fd1249bfe3a9d
MonsieurAmengood <137416510+MonsieurAmengood2@users.noreply.github.com>
2025-05-02 18:13:13 +0100

    Update do Dashboard.jsx
    Adicionados Botões ao invés de Links no Dashboard.jsx

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
-import "../styles/dashboard-backoffice.css"
-import { Link } from 'react-router-dom'; // Importe o Link
+import "../styles/dashboard-backoffice.css";
+import { useNavigate } from 'react-router-dom'; // Importe useNavigate
 
 const Dashboard = () => {
-
-    const nome = "Diogo Larangeira"; 
+  const navigate = useNavigate(); // Inicialize o hook useNavigate
+  const nome = "Diogo Larangeira"; 
 
   return (
     <div className="area">
       <h1 className="titulo">Bem-vindo <span className="destaque">{nome}</span></h1>
       <div className="botoes-container">
-         <Link to="/HorariosESGT" className="botao">Horários ESGT</Link>
-         <Link to="/HorariosESTT" className="botao">Horários ESTT</Link>
-         <Link to="/HorariosESTA" className="botao">Horários ESTA</Link>
+        {/* Botão Horários ESGT */}
+        <button 
+          className="botao" 
+          onClick={() => navigate('/HorariosESGT')}
+        >
+          Horários ESGT
+        </button>
+
+        {/* Botão Horários ESTT */}
+        <button 
+          className="botao" 
+          onClick={() => navigate('/HorariosESTT')}
+        >
+          Horários ESTT
+        </button>
+
+        {/* Botão Horários ESTA */}
+        <button 
+          className="botao" 
+          onClick={() => navigate('/HorariosESTA')}
+        >
+          Horários ESTA
+        </button>
       </div>
     </div>
   );
