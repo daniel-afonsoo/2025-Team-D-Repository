13750a0ead2384fe1dccf376ccdf350f1b9f2889
Guilherme Simões <137619185+GuilhermeSimoes1@users.noreply.github.com>
2025-05-08 15:08:12 +0100

    Rate limit com email
    - Rate limit com email e IP público, caso um email falhe 5 vezes fica bloqueado e caso um ip público falhe 100 vezes também o fica.

--- server/endpoints/auth-endpoints.js
+++ server/endpoints/auth-endpoints.js
@@ -7,12 +7,43 @@ const rateLimit = require('express-rate-limit')
 
 const authLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 5,
+    max: 6,
     message: 'Demasiadas tentativas de login. Tente novamente mais tarde.'
 })
 
+const loginAttempts = new Map();
+
+const emailLimiter = (req, res, next) => {
+    const {email} = req.body;
+    if(!email){
+        return res.status(400).json({message: 'Email is required'});
+    }
+
+    const now = Date.now();
+    const limitWindowMs = 15 * 60 * 1000;
+    const maxAttempts = 5;
+
+    const attempts = loginAttempts.get(email) || { count: 0, lastAttempt: now };
+    
+    if(now - attempts.firstAttempt > limitWindowMs) {
+        attempts.count = 0;
+    }
+    
+    attempts.count += 1;
+    attempts.lastAttempt = now;
+
+    
+    loginAttempts.set(email, attempts);
+
+    if(attempts.count > maxAttempts) {
+        return res.status(429).json({message: 'Demasiadas tentativas de login. Tente novamente mais tarde.'});
+    }
+
+    next();
+}
+
 //FUNCIONA
-router.post('/auth/login', authLimiter, async (req,res) => {
+router.post('/auth/login', authLimiter, emailLimiter, async (req,res) => {
     const {email,password} = req.body
     
     try {
@@ -28,6 +59,8 @@ router.post('/auth/login', authLimiter, async (req,res) => {
         const match = await bcrypt.compare(password, results[0].Password)
         
         if (match) {
+            // Resetar tentativas no sucesso do login
+            loginAttempts.delete(email);
             return res.status(200).json({message: 'Login successful'})
         } else {
             return res.status(401).json({message: 'Invalid email or password'})
