c711bcb2296010a5b8f955b42eed98375e623751
Rúben Dias <110482980+aspaceusername@users.noreply.github.com>
2025-05-10 17:52:12 +0100

    Bugfix filtros

--- client/src/components/horarios/Filtros.jsx
+++ client/src/components/horarios/Filtros.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-function Filtros({ escola,setEscola,docente, setDocente, sala, setSala, turma, setTurma, uc, setUc, curso, setCurso, ano, setAno }) {
+function Filtros({ escola, setEscola, docente, setDocente, sala, setSala, turma, setTurma, uc, setUc, curso, setCurso, ano, setAno }) {
   const [docentes, setDocentes] = useState([]);
   const [salas, setSalas] = useState([]);
   const [turmas, setTurmas] = useState([]);
@@ -52,68 +52,89 @@ function Filtros({ escola,setEscola,docente, setDocente, sala, setSala, turma, s
       <div className="filtros">
         <h3>Filtros</h3>
 
-        <select onChange={(e) => setEscola(e.target.value)} value={escola}>
-          <option value="">Escolher Escola</option>
-          {escolas.map((escola, index) => (
-            <option key={index} value={escola}>
-              {escola}
-            </option>
-          ))}
-        </select>
+        <label>
+          Escola:
+          <select onChange={(e) => setEscola(e.target.value)} value={escola}>
+            <option value="">Escolher Escola</option>
+            {escolas.map((escola, index) => (
+              <option key={index} value={escola}>
+                {escola}
+              </option>
+            ))}
+          </select>
+        </label>
 
-        <select onChange={(e) => setDocente(e.target.value)} value={docente}>
-          <option value="">Escolher Docente</option>
-          {docentes.map((docente, index) => (
-            <option key={index} value={docente}>
-              {docente}
-            </option>
-          ))}
-        </select>
+        <label>
+          Docente:
+          <select onChange={(e) => setDocente(e.target.value)} value={docente}>
+            <option value="">Escolher Docente</option>
+            {docentes.map((docente, index) => (
+              <option key={index} value={docente}>
+                {docente}
+              </option>
+            ))}
+          </select>
+        </label>
 
-        <select onChange={(e) => setSala(e.target.value)} value={sala}>
-          <option value="">Escolher Sala</option>
-          {salas.map((sala, index) => (
-            <option key={index} value={sala}>
-              {sala}
-            </option>
-          ))}
-        </select>
+        <label>
+          Sala:
+          <select onChange={(e) => setSala(e.target.value)} value={sala}>
+            <option value="">Escolher Sala</option>
+            {salas.map((sala, index) => (
+              <option key={index} value={sala}>
+                {sala}
+              </option>
+            ))}
+          </select>
+        </label>
 
-        <select onChange={(e) => setTurma(e.target.value)} value={turma}>
-          <option value="">Escolher Turma</option>
-          {turmas.map((turma, index) => (
-            <option key={index} value={turma}>
-              {turma}
-            </option>
-          ))}
-        </select>
+        <label>
+          Turma:
+          <select onChange={(e) => setTurma(e.target.value)} value={turma}>
+            <option value="">Escolher Turma</option>
+            {turmas.map((turma, index) => (
+              <option key={index} value={turma}>
+                {turma}
+              </option>
+            ))}
+          </select>
+        </label>
 
-        <select onChange={(e) => setUc(e.target.value)} value={uc}>
-          <option value="">Escolher UC</option>
-          {ucs.map((uc, index) => (
-            <option key={index} value={uc}>
-              {uc}
-            </option>
-          ))}
-        </select>
+        <label>
+          Unidade Curricular (UC):
+          <select onChange={(e) => setUc(e.target.value)} value={uc}>
+            <option value="">Escolher UC</option>
+            {ucs.map((uc, index) => (
+              <option key={index} value={uc}>
+                {uc}
+              </option>
+            ))}
+          </select>
+        </label>
 
-        <select onChange={(e) => setCurso(e.target.value)} value={curso}>
-          <option value="">Escolher Curso</option>
-          {cursos.map((curso, index) => (
-            <option key={index} value={curso}>
-              {curso}
-            </option>
-          ))}
-        </select>
+        <label>
+          Curso:
+          <select onChange={(e) => setCurso(e.target.value)} value={curso}>
+            <option value="">Escolher Curso</option>
+            {cursos.map((curso, index) => (
+              <option key={index} value={curso}>
+                {curso}
+              </option>
+            ))}
+          </select>
+        </label>
 
-        <select onChange={(e) => setAno(e.target.value)} value={ano}>
-          <option value="">Escolher Ano</option>
-          {anos.map((ano, index) => (
-            <option key={index} value={ano}>
-              {ano}
-            </option>
-          ))}
-        </select>
+        <label>
+          Ano:
+          <select onChange={(e) => setAno(e.target.value)} value={ano}>
+            <option value="">Escolher Ano</option>
+            {anos.map((ano, index) => (
+              <option key={index} value={ano}>
+                {ano}
+              </option>
+            ))}
+          </select>
+        </label>
       </div>
     </div>
   );
