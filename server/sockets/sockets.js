2354a48c2c2a64bab4fe681c0b783bb6dc01945d
Benquerer <andrebenquerer@gmail.com>
2025-05-15 16:43:31 +0100

    Added class scheduling on DB

--- server/sockets/sockets.js
+++ server/sockets/sockets.js
@@ -2,7 +2,7 @@
 const { Server } = require('socket.io');
 const { setSocketIOInstance, logToClient, getBufferedLogs } = require('../utils/logger'); // import logger instance
 const pool = require('../db/connection.js'); 
-const { formatAulaRow } = require('../utils/utils.js'); 
+const { formatAulaRow, addAulaToDB, removeAulaFromDB, updateAulaInDB } = require('../utils/utils.js'); 
 
 const setupSockets = (server) => {
     const io = new Server(server, {
@@ -48,55 +48,78 @@ const setupSockets = (server) => {
         console.log("Enviadas pela primeira vez")
         
 
-        // add aula to server
-        socket.on("add-aula", (data) => {
-            // check if the class is valid
-            let valid = true
-            if (valid) {  // always true for now, add validation logic later (post to database)
-                //add id to new aula
-                let newId = schedule[schedule.length - 1].Cod_Aula + 1   
-                data.newAula.Cod_Aula = newId
-                schedule.push(data.newAula) // add to server schedule
-                console.log("Aula added: ", data.newAula)
-                // broadcast to all clients update
-                io.emit("update-aulas", { newAulas: schedule })
-            } else {
-                // error in adding aula
-                console.log("Error adding aula: ", data.newAulas)
-                socket.emit("add-aula-error", { message: "Esta aula não pode ser adicionada." })
-            }
-        })
+        // Add aula
+        socket.on("add-aula", async (data) => {
+            try {
+                const newId = schedule.length > 0 ? schedule[schedule.length - 1].Cod_Aula + 1 : 1;
+                data.newAula.Cod_Aula = newId;
 
-        // remove aula from server
-        socket.on("remove-aula", (data) => {
-            // check if the class is in the schedule
-            let aulaExists = schedule.some((aula) => aula.Cod_Aula === data.codAula)
+                ////////////////////////////////////////////////////////////////////////////////////////////////
+                /////////////////////// ADDING STATIC DATA ON MISSING FIELDS FOR TESTING ///////////////////////
+                ////////////////////////////////////////////////////////////////////////////////////////////////
+
+                data.newAula.docente = 1;
+                data.newAula.location = 1;
+                data.newAula.turma = 1;
+                data.newAula.course = 1;
+                data.newAula.anoSem = 1;
+
+                ////////////////////////////////////////////////////////////////////////////////////////////////
+                ////////////////////////////////////////////////////////////////////////////////////////////////
+                ////////////////////////////////////////////////////////////////////////////////////////////////
+                
+ 
+                schedule.push(data.newAula);
+                await addAulaToDB(data.newAula);
+
+                console.log("Aula added: ", data.newAula);
+                io.emit("update-aulas", { newAulas: schedule });
+            } catch (err) {
+                console.error("Error adding aula:", err);
+                socket.emit("add-aula-error", { message: "Erro ao adicionar aula ao banco de dados." });
+            }
+        });
+
+        // Remove aula
+        socket.on("remove-aula", async (data) => {
+            const aulaExists = schedule.some(aula => aula.Cod_Aula === data.codAula);
             if (aulaExists) {
-                schedule = schedule.filter((aula) => aula.Cod_Aula !== data.codAula)
-                console.log("Aula removed: ", data.codAula)
-                // broadcast to all clients update
-                io.emit("update-aulas", { newAulas: schedule })
-            } else {
-                // error in removing aula
-                console.log("Error removing aula: ", data.codAula)
-                socket.emit("remove-aula-error", { message: "Esta aula não pode ser removida." })
-            }
-        })
+                try {
+                    schedule = schedule.filter(aula => aula.Cod_Aula !== data.codAula);
+                    await removeAulaFromDB(data.codAula);
 
-        socket.on("update-aula", (data) => {
-            console.log(data.newDay)
-            console.log(data.newStart)
-            // update aula in schedule
-            let aulaToUpdate = schedule.find((aula) => aula.Cod_Aula === data.codAula);
-            if (aulaToUpdate) {
-                aulaToUpdate.day = data.newDay
-                aulaToUpdate.start = data.newStart
-                console.log("Aula updated: ", aulaToUpdate)
-                // broadcast to all clients update
-                io.emit("update-aulas", { newAulas: schedule })
+                    console.log("Aula removed: ", data.codAula);
+                    io.emit("update-aulas", { newAulas: schedule });
+                } catch (err) {
+                    console.error("Error removing aula:", err);
+                    socket.emit("remove-aula-error", { message: "Erro ao remover aula do banco de dados." });
+                }
             } else {
-                console.log("Error updating aula: ", codaula)
-                socket.emit("update-aula-error", { message: "Esta aula não pode ser atualizada." })
+                console.log("Aula not found: ", data.codAula);
+                socket.emit("remove-aula-error", { message: "Esta aula não pode ser removida." });
+            }
+        });
+
+        // Update aula
+        socket.on("update-aula", async (data) => {
+            const aulaToUpdate = schedule.find(aula => aula.Cod_Aula === data.codAula);
+            if (aulaToUpdate) {
+                try {
+                    aulaToUpdate.day = data.newDay;
+                    aulaToUpdate.start = data.newStart;
+                    aulaToUpdate.end = data.newEnd;
+
+                    await updateAulaInDB(data.codAula, data.newDay, data.newStart);
+
+                    console.log("Aula updated: ", aulaToUpdate);
+                    io.emit("update-aulas", { newAulas: schedule });
+                } catch (err) {
+                    console.error("Error updating aula:", err);
+                    socket.emit("update-aula-error", { message: "Erro ao atualizar aula no banco de dados." });
+                }
+            } else {
+                console.log("Aula not found for update: ", data.codAula);
+                socket.emit("update-aula-error", { message: "Esta aula não pode ser atualizada." });
             }
         });
 
--- server/utils/utils.js
+++ server/utils/utils.js
@@ -1,16 +1,51 @@
+const pool = require('../db/connection.js'); 
 
 function formatAulaRow(row) {
     return {
         Cod_Aula: row.Cod_Aula,
+        docente : row.Cod_Docente,
+        location: row.Cod_Sala,
+        turma: row.Cod_Turma,
+        subject: row.Cod_Uc,
+        course: row.Cod_Curso,
+        anoSem: row.AnoSemestre,
         day: row.Dia,
         start: row.Inicio,
         end: row.Fim,
-        subject: row.Cod_Uc,
-        location: row.Cod_Sala,
         duration: row.Duration
     };
 }
 
+// Add aula to DB
+const addAulaToDB = async (aula) => {
+    const query = `
+        INSERT INTO aula (
+            Cod_Aula, Cod_Docente, Cod_Sala, Cod_Turma, Cod_Uc,
+            Cod_Curso, AnoSemestre, Dia, Inicio, Fim, Duration
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+    const values = [
+        aula.Cod_Aula, aula.docente, aula.location, aula.turma, aula.subject, aula.course, aula.anoSem, aula.day, aula.start, aula.end, aula.duration
+    ];
+
+    await pool.promise().query(query, values);
+};
+
+// Remove aula from DB
+const removeAulaFromDB = async (codAula) => {
+    const query = `DELETE FROM aula WHERE Cod_Aula = ?`;
+    await pool.promise().query(query, [codAula]);
+};
+
+// Update only day/start time (can be expanded)
+const updateAulaInDB = async (codAula, newDay, newStart) => {
+    const query = `UPDATE aula SET Dia = ?, Inicio = ? WHERE Cod_Aula = ?`;
+    await pool.promise().query(query, [newDay, newStart, codAula]);
+};
+
 module.exports = {
-    formatAulaRow
+    formatAulaRow,
+    addAulaToDB,
+    removeAulaFromDB,
+    updateAulaInDB
 };
